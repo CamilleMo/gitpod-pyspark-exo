36edb2866169a1434320f707f6087be2f6b57340
Camille Moatti <camille.moatti@gmail.com>
2023-04-04 14:54:54 +0000

    add instructions

--- main.py
+++ main.py
@@ -14,4 +14,35 @@ transactions_df = spark.read \
 # Display the first few rows of the DataFrame
 transactions_df.show()
 
+# Count the total number of transactions in the dataset.
 
+# Calculate the sum of total_amount for all transactions.
+print("sum of total amount :")
+
+# Find the average total_amount per transaction.
+print("avg of total amount :")
+
+# Find the total_amount spent by each customer.
+print("Total amount by customer :")
+
+# Count the number of transactions for each product.
+print("count of transactions by products")
+
+# Calculate the daily total revenue.
+print("daily total revenue")
+
+# Find the customer who spent the most money.
+print("customer by spending")
+
+
+# Join the transactions DataFrame with the customers and products DataFrames.
+
+
+
+# Calculate the total revenue per product category.
+
+# Find the most popular product in each category based on the number of transactions.
+
+# Identify the top 3 customers with the highest spending in each category.
+
+# Calculate the age distribution of customers who made transactions.
